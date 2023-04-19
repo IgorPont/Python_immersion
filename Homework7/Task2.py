d3e05d9a132bd48155408f710c1bad6ebe62e62a
igor_pont <pontigor11@gmail.com>
2023-04-19 20:22:33 +0300

    Added task 2

---
+++ Homework7/Task2.py
@@ -0,0 +1,7 @@
+"""
+Соберите из созданных на уроке и в рамках домашнего задания функций пакет для работы с файлами.
+"""
+from Seminar7.files_work import file_generator
+from Seminar7.files_work import get_file_numbers
+from Seminar7.files_work import mult_num_name
+from Seminar7.files_work import pseudonym_generator
